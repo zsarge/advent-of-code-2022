faf8acdec0c314194bc541ff3c8a4f1398441e3e
Zack Sargent <zackarywsargent@gmail.com>
2022-12-06 00:30:08 -0500

    update solve 3

--- solutions/03/solve3.rb
+++ solutions/03/solve3.rb
@@ -6,21 +6,6 @@
 
 require 'set'
 
-class Rucksack
-  attr_reader :left, :right
-
-  def initialize contents
-    halfway = contents.size/2 
-    @left = Set.new(contents[...halfway].chars)
-    @right = Set.new(contents[halfway..].chars)
-  end
-
-  def difference 
-    @left.intersection @right
-  end
-end
-
-
 def priority letter
   case letter
   when ('a'..'z')
@@ -30,17 +15,24 @@ def priority letter
   end 
 end
 
-FILE_NAME = "input3.txt"
-# FILE_NAME = "test.txt"
-INPUTS = File.readlines(FILE_NAME).map(&:chomp)
+INPUTS = File.readlines("input3.txt").map(&:chomp)
 
 # part 1
-p INPUTS.map{|line| Rucksack.new line}.flat_map{|rucksack|
-  rucksack.difference.map{ priority _1 }
-}.sum
+p INPUTS
+  .flat_map{ |contents| 
+    halfway = contents.size/2 
+    left = Set.new(contents[...halfway].chars)
+    right = Set.new(contents[halfway..].chars)
+    left.intersection(right).map{ priority _1 }
+  }
+  .sum
 
 # part 2
-p INPUTS.map{|line| Set.new line.chars}.each_slice(3).map{|(elf_1, elf_2, elf_3)|
-  elf_1.intersection(elf_2).intersection(elf_3)
-}.flat_map{|set| set.map {priority _1}}.sum
+p INPUTS
+  .map{ |line| Set.new line.chars }
+  .each_slice(3)
+  .flat_map{ |(elf_1, elf_2, elf_3)|
+    elf_1.intersection(elf_2).intersection(elf_3).map {priority _1}
+  }
+  .sum
 
