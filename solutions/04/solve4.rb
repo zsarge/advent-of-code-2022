6294cefb6d31a5969e695596ad37319ce9c0e360
Zack Sargent <zackarywsargent@gmail.com>
2022-12-07 14:25:52 -0500

    solve day 4 parts 1 and 2

---
+++ solutions/04/solve4.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+# https://adventofcode.com/2022/day/4
+# Run with: 'ruby solve4.rb'
+# using Ruby 3.1.2
+# by Zack Sargent
+
+
+class Elf < Struct.new(:start, :end)
+  def fully_contains? other
+    self.start <= other.start && other.end <= self.end
+  end
+
+  def overlaps? other
+    self.start <= other.start && other.start <= self.end
+  end
+end
+
+INPUTS = File.readlines("input4.txt", chomp: true).map{ |line|
+  /(\d+)-(\d+),(\d+)-(\d+)/ =~ line
+  [Elf.new($1.to_i, $2.to_i), Elf.new($3.to_i, $4.to_i)]
+}
+
+p INPUTS.filter{ |(elf1, elf2)|
+    elf1.fully_contains?(elf2) || elf2.fully_contains?(elf1)
+  }.size
+
+p INPUTS.filter{ |(elf1, elf2)|
+    elf1.overlaps?(elf2) || elf2.overlaps?(elf1)
+  }.size
+
