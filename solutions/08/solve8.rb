4be14438b87b60341a26d9f9a1d7fd7c92653031
Zack Sargent <zackarywsargent@gmail.com>
2022-12-08 15:24:39 -0500

    refine solution

--- solutions/08/solve8.rb
+++ solutions/08/solve8.rb
@@ -6,44 +6,39 @@
 
 TREES = File.readlines("input8.txt", chomp: true).map{_1.chars.map(&:to_i)}
 
-def viewing_distances x, y
+def viewing_distances x, y # TODO: Yield (range.size - 1)
   (up, down) = [(y-1).downto(0), (y+1...TREES.size)].map { |range|
-    count = 0
-    range.each do |y2|
-      count += 1 
-      break unless TREES[y2][x] < TREES[y][x] 
-    end
-    count
+	range.find_index {|y2| TREES[y2][x] >= TREES[y][x] } || yield(range)
   }
   (left, right) = [(x-1).downto(0), (x+1...TREES[0].size)].map { |range|
-    count = 0
-    range.each do |x2|
-      count += 1 
-      break unless TREES[y][x2] < TREES[y][x] 
-    end
-    count
+	range.find_index {|x2| TREES[y][x2] >= TREES[y][x] } || yield(range)
   }
 
-  [up, left, right, down]
+  [up, left, right, down].map{ _1 + 1 }
 end
 
-
 def scenic_score x, y
-  viewing_distances(x, y).reduce(&:*)
+  viewing_distances(x, y){ |range| range.size - 1 }.reduce(&:*)
 end
 
 def visible? x, y
-  viewing_distances(x, y).any? { _1 > 0}
+  # trees that can see the edge have infinite viewing distance
+  viewing_distances(x, y){ Float::INFINITY }.include?(Float::INFINITY) 
 end
 
 # could be replaced by Enumerator.product in later Ruby versions
-center_points = (1...TREES.size-1).to_a.product((1...TREES[0].size-1).to_a) 
+center_points = (1...TREES.size-1).to_a.product((1...TREES[0].size-1).to_a)
 
+
+# part 1
 visible_edges = (TREES.size * 2) + (TREES[0].size * 2) - 4
+
+# puts "visible_edges = #{visible_edges} + ? = "
 p visible_edges + center_points.count {|(x, y)| visible?(x, y)}
 
-# as opposed to calling .map then .max, .max_by avoids generating a new array, 
+# part 2
+# As opposed to calling .map then .max, .max_by avoids generating a new array,
 # and lets me pretend like this is totally optimized :)
 p center_points
-  .max_by {|(x, y)| scenic_score(x, y)} 
+  .max_by {|(x, y)| scenic_score(x, y)}
   .then {|(x, y)| scenic_score(x, y)}
