d0e58c5ef788095b80aa1cc2ac0bcac914a11ec2
Zack Sargent <zackarywsargent@gmail.com>
2022-12-08 11:42:07 -0500

    generate README.md

--- solutions/08/solve8.rb
+++ solutions/08/solve8.rb
@@ -1,12 +1,10 @@
 #!/usr/bin/env ruby
 # https://adventofcode.com/2022/day/8
 # Run with: 'ruby solve8.rb'
-# using Ruby 3.1.2
+# using Ruby 3.1.3
 # by Zack Sargent
 
-FILE_NAME = "input8.txt"
-# FILE_NAME = "test.txt"
-TREES = File.readlines(FILE_NAME, chomp: true).map{_1.chars.map(&:to_i)}
+TREES = File.readlines("input8.txt", chomp: true).map{_1.chars.map(&:to_i)}
 
 def viewing_distances x, y
   (up, down) = [(y-1).downto(0), (y+1...TREES.size)].map { |range|
@@ -31,26 +29,21 @@ end
 
 
 def scenic_score x, y
-  viewing_distances(x,y).reduce(&:*)
+  viewing_distances(x, y).reduce(&:*)
 end
 
 def visible? x, y
-  viewing_distances(x,y).any? { _1 > 0}
+  viewing_distances(x, y).any? { _1 > 0}
 end
 
-center_points = Enumerator.new do |en| # could be replaced by Enumerator.product in later versions
-  (1...TREES.size-1).each do |y|
-    (1...TREES[0].size-1).each do |x|
-      en << [x,y]
-    end
-  end
-end
+# could be replaced by Enumerator.product in later Ruby versions
+center_points = (1...TREES.size-1).to_a.product((1...TREES[0].size-1).to_a) 
 
 visible_edges = (TREES.size * 2) + (TREES[0].size * 2) - 4
-p visible_edges + center_points.count {|(x,y)| visible?(x,y)}
+p visible_edges + center_points.count {|(x, y)| visible?(x, y)}
 
 # as opposed to calling .map then .max, .max_by avoids generating a new array, 
 # and lets me pretend like this is totally optimized :)
 p center_points
-  .max_by {|(x,y)| scenic_score(x,y)} 
-  .then {|(x,y)| scenic_score(x,y)}
+  .max_by {|(x, y)| scenic_score(x, y)} 
+  .then {|(x, y)| scenic_score(x, y)}
