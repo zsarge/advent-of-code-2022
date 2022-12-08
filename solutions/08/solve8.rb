24b77c67db0ed28432a08be7f2f66df682973ed8
Zack Sargent <zackarywsargent@gmail.com>
2022-12-08 11:31:13 -0500

    clean up day 8 solution

--- solutions/08/solve8.rb
+++ solutions/08/solve8.rb
@@ -8,32 +8,7 @@ FILE_NAME = "input8.txt"
 # FILE_NAME = "test.txt"
 TREES = File.readlines(FILE_NAME, chomp: true).map{_1.chars.map(&:to_i)}
 
-def visible? x, y
-  # visible from top or bottom
-  return true if [(0...y), (y+1...TREES.size)].map { |range|
-    range.all?{ |y2| TREES[y2][x] < TREES[y][x] }
-  }.any?
-
-  # visible from left or right
-  [(0...x), (x+1...TREES[0].size)].map { |range|
-    range.all?{ |x2| TREES[y][x2] < TREES[y][x] }
-  }.any?
-end
-
-def part_1
-  visible_edges = (TREES.size * 2) + (TREES[0].size * 2) - 4
-
-  center = (1...TREES.size-1).sum { |y|
-    (1...TREES[0].size-1).count { |x|
-      visible?(x,y)
-    }
-  }
-
-  center + visible_edges
-end
-
-
-def viewing_distance x, y
+def viewing_distances x, y
   (up, down) = [(y-1).downto(0), (y+1...TREES.size)].map { |range|
     count = 0
     range.each do |y2|
@@ -56,10 +31,26 @@ end
 
 
 def scenic_score x, y
-  viewing_distance(x,y).reduce(&:*)
+  viewing_distances(x,y).reduce(&:*)
 end
 
+def visible? x, y
+  viewing_distances(x,y).any? { _1 > 0}
+end
 
-p (1...TREES.size-1).to_a.product((1...TREES[0].size-1).to_a)
-  .map{|(x,y)| scenic_score(x,y)}
-  .max
+center_points = Enumerator.new do |en| # could be replaced by Enumerator.product in later versions
+  (1...TREES.size-1).each do |y|
+    (1...TREES[0].size-1).each do |x|
+      en << [x,y]
+    end
+  end
+end
+
+visible_edges = (TREES.size * 2) + (TREES[0].size * 2) - 4
+p visible_edges + center_points.count {|(x,y)| visible?(x,y)}
+
+# as opposed to calling .map then .max, .max_by avoids generating a new array, 
+# and lets me pretend like this is totally optimized :)
+p center_points
+  .max_by {|(x,y)| scenic_score(x,y)} 
+  .then {|(x,y)| scenic_score(x,y)}
