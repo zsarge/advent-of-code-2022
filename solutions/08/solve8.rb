c10308354bc1af9c0812895da4c440eeb3012de0
Zack Sargent <zackarywsargent@gmail.com>
2022-12-08 15:29:56 -0500

    update comments

--- solutions/08/solve8.rb
+++ solutions/08/solve8.rb
@@ -4,14 +4,14 @@
 # using Ruby 3.1.3
 # by Zack Sargent
 
-TREES = File.readlines("input8.txt", chomp: true).map{_1.chars.map(&:to_i)}
+TREES = File.readlines("input8.txt", chomp: true).map{ _1.chars.map(&:to_i) }
 
-def viewing_distances x, y # TODO: Yield (range.size - 1)
+def viewing_distances x, y
   (up, down) = [(y-1).downto(0), (y+1...TREES.size)].map { |range|
-	range.find_index {|y2| TREES[y2][x] >= TREES[y][x] } || yield(range)
+	range.find_index{ |y2| TREES[y2][x] >= TREES[y][x] } || yield(range)
   }
   (left, right) = [(x-1).downto(0), (x+1...TREES[0].size)].map { |range|
-	range.find_index {|x2| TREES[y][x2] >= TREES[y][x] } || yield(range)
+	range.find_index{ |x2| TREES[y][x2] >= TREES[y][x] } || yield(range)
   }
 
   [up, left, right, down].map{ _1 + 1 }
@@ -23,7 +23,7 @@ end
 
 def visible? x, y
   # trees that can see the edge have infinite viewing distance
-  viewing_distances(x, y){ Float::INFINITY }.include?(Float::INFINITY) 
+  viewing_distances(x, y){ Float::INFINITY }.include?(Float::INFINITY)
 end
 
 # could be replaced by Enumerator.product in later Ruby versions
@@ -33,11 +33,10 @@ center_points = (1...TREES.size-1).to_a.product((1...TREES[0].size-1).to_a)
 # part 1
 visible_edges = (TREES.size * 2) + (TREES[0].size * 2) - 4
 
-# puts "visible_edges = #{visible_edges} + ? = "
-p visible_edges + center_points.count {|(x, y)| visible?(x, y)}
+p visible_edges + center_points.count{ |(x, y)| visible?(x, y)}
 
 # part 2
-# As opposed to calling .map then .max, .max_by avoids generating a new array,
+# As opposed to calling .map and .max, .max_by and .then avoids generating a new array,
 # and lets me pretend like this is totally optimized :)
 p center_points
   .max_by {|(x, y)| scenic_score(x, y)}
